de1321dff34a03b530c3ea41053a5485021c6ecd
Soumadip <39thdecember@gmail.com>
2024-07-29 02:51:13 +0530

    auth provider added

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+'use client';
+
+import { SignOutButton } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
+import Link from "next/link";
 
 function SignOutLink() {
+  const {toast} = useToast();
+
+  function handleLogOut(){
+    toast({description: "Logout Successful!"})
+  }
+
   return (
-    <div>
-      SignOutLink
-    </div>
+    <SignOutButton >
+      <Link href="/" className="w-full text-left" onClick={handleLogOut}>Logout</Link>
+    </SignOutButton>
   )
 }
 
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,10 +1,17 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { LuUser2 } from "react-icons/lu";
 
-function UserIcon() {
+async function UserIcon() {
+//const {userId} = auth()
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+  if(profileImage) {
+    return <img src={profileImage} alt="user_img" className="rounded-full w-6 object-cover"/>;
+  }
+
   return (
-    <div>
-      UserIcon
-    </div>
+    <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white"/>
   )
 }
 
-export default UserIcon
+export default UserIcon;
